2dbf0dba81237a6469777817f472a1fb71e3ba1b
Mudassir <mudassir.rafiq.007@gmail.com>
2022-07-30 21:38:23 +0500

    Code Refined (Single App.tsx function divided into components

---
+++ components/FlatButton.tsx
@@ -0,0 +1,37 @@
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native"
+
+interface propType{
+    buttonText: string;
+    buttonFunction: ()=> void;
+}
+
+export default function FlatButton (props: propType) {
+
+    return (
+        <View>
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={props.buttonFunction}
+        >
+          <Text style={styles.buttonText}>{props.buttonText}</Text>
+        </TouchableOpacity>
+      </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    buttonText: {
+        color: "white",
+        fontSize: 20,
+        fontWeight: "bold",
+      },
+    
+      buttonContainer: {
+        backgroundColor: "#e33636",
+        padding: 10,
+        width: 100,
+        alignItems: "center",
+        margin: 10,
+        borderRadius: 10,
+      },
+})
